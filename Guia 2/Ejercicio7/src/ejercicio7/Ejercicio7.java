413f3686b4a86c4fb6154f72be108a71718aa7d9
“brunozenobio” <brunozenobio4@gmail.com>
2023-03-14 19:17:04 -0300

    Guia2 completa sin extras

--- Guia 2/Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Guia 2/Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -5,17 +5,56 @@
  */
 package ejercicio7;
 
+import java.util.Scanner;
+
 /**
- *
+ *Realizar un programa que simule el funcionamiento de un dispositivo RS232, este tipo de dispositivo 
+ * lee cadenas enviadas por el usuario. Las cadenas deben llegar con un formato fijo: 
+ * tienen que ser de un máximo de 5 caracteres de largo, el primer carácter tiene que ser
+ * X y el último tiene que ser una O.Las secuencias leídas que respeten el formato se 
+ * consideran correctas, la secuencia especial “&&&&&” marca el final de los envíos (llamémosla FDE), y 
+ * toda secuencia distinta de FDE, que no respete el formato se considera incorrecta.Al finalizar el proceso, 
+ * se imprime un informe indicando la cantidad de lecturas correctas e incorrectas recibidas. Para resolver el 
+ * ejercicio deberá investigar cómo se utilizan las siguientes funciones de Java Substring(), Length(), equals().
  * @author bruno
  */
 public class Ejercicio7 {
-
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        //RS232
+        
+        String secuencia;//Defino la variable para introducir las secuencias
+        int sumaCorrect,sumaIncorrect; //Suma la cantidad de lecturas correctas e incorrectas
+        
+        Scanner read = new Scanner(System.in);//Defino read para leer datos
+        
+        System.out.println("Este programa lee cadenas RS232 y define si son correctas o no");
+        System.out.println("Recuerde que estas inicician con 'X' y finalizan con 'O' y cuentan con 5 carcteres ");
+        System.out.println("Y finaliza con &&&&&");
+        sumaCorrect = 0;
+        sumaIncorrect = 0;
+        do{
+            System.out.println("Ingrese una secuencia RS232");
+            secuencia = read.nextLine();
+            if(secuencia.length() == 5){
+                if(secuencia.substring(0,1).equals("X") && secuencia.substring(secuencia.length() -1).equals("O")){
+                    sumaCorrect += 1;
+                }else{
+                    sumaIncorrect += 1; 
+                }
+            }else{
+                sumaIncorrect += 1; 
+            }
+        }while(!secuencia.equals("&&&&&"));
+        
+        System.out.println("El programa a finalizado y este tiene");
+        System.out.println(sumaCorrect - 1 + " :secuencias correctas ");
+        System.out.println(sumaIncorrect - 1 + " :secuencias incorrectas ");
+        
     }
     
 }
--- Guia 2/Ejercicio8/src/ejercicio8/Ejercicio8.java
+++ Guia 2/Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -4,9 +4,11 @@
  * and open the template in the editor.
  */
 package ejercicio8;
-
+import java.util.Scanner;
 /**
- *
+ *Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. 
+ * Por ejemplo, si el cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
+
  * @author bruno
  */
 public class Ejercicio8 {
@@ -16,6 +18,32 @@ public class Ejercicio8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Escribir cuadrado
+        
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Ingrese el numero de asteriscos del cuadrado");
+        int n = read.nextInt();
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 || i == n - 1 || j == 0 || j == n - 1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+        
+        
+        
+    
+        
+        
+        
     }
     
 }
